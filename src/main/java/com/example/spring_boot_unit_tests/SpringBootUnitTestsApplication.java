eda625b470a25f7f8495a6ca8f6136d0f020b0b5
deep4109 <singhdeepankar41@gmail.com>
2024-10-02 15:30:53 +0530

    first

---
+++ src/main/java/com/example/spring_boot_unit_tests/SpringBootUnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_unit_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnitTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_unit_tests/SpringBootUnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_unit_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
